1cd93722ecd8bebb2712126bd60f32b06d61b0ea
Eehrst <114171408+Eehrst@users.noreply.github.com>
2024-06-26 15:31:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: " Os jogos exercem um papel fundamental na vida humana, proporcionando não apenas entretenimento, mas também oportunidades de aprendizado, socialização e desenvolvimento pessoal. Desde os primórdios da civilização, os jogos têm sido uma forma de explorar habilidades físicas e mentais, desafiando limites e estimulando a criatividade. Seja um simples jogo de tabuleiro em família ou um complexo videogame multiplayer online, cada experiência de jogo oferece uma jornada única de descoberta e diversão. Qual é o seu tipo de jogo preferido?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: " Estratégia",
+                afirmacao: " *Os jogos oferecem uma variedade de vantagens significativas para os jogadores de todas as idades. Eles estimulam o desenvolvimento cognitivo, melhorando habilidades como resolução de problemas e raciocínio estratégico.  "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Ação ",
+                afirmacao: "*os jogos ofereçam muitos benefícios, há também desvantagens a serem consideradas. O excesso de tempo dedicado aos jogos pode levar ao isolamento social, diminuindo as interações pessoais e afetando relacionamentos.  "
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: " O que mais te atrai em um jogo?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: " História envolvente ",
+                afirmacao: " *promovem a colaboração e o trabalho em equipe, essenciais tanto em jogos multiplayer quanto em atividades cotidianas. "
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: " Jogabilidade desafiadora",
+                afirmacao: " *Alguns jogos violentos podem desensibilizar os jogadores à violência real, especialmente quando não há um equilíbrio com experiências positivas. "
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: " Como você costuma jogar mais frequentemente? ",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: " Sozinho",
+                afirmacao: " *Jogos educativos podem ser uma ferramenta eficaz no ensino, tornando o aprendizado mais envolvente e interativo. "
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: " Com amigos ou online com desconhecidos ",
+                afirmacao: "*A dependência de jogos pode prejudicar o desempenho acadêmico e profissional devido à falta de foco em outras áreas da vida. "
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: " Qual plataforma de jogo você prefere? ",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: " Consoles (PlayStation, Xbox) ",
+                afirmacao: " *Eles também proporcionam um escape saudável do estresse diário, ajudando a relaxar e a aliviar a ansiedade.  "
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "PC ou celular.",
+                afirmacao: " *Problemas de saúde física, como dores crônicas e lesões por esforço repetitivo, são comuns devido a longas sessões de jogo sem pausas adequadas. "
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O que você valoriza mais em um jogo? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: " Gráficos e tecnologia avançada ",
+                afirmacao: "  *Por fim, os jogos podem ser uma forma de expressão artística, através de gráficos impressionantes, trilhas sonoras emocionantes e narrativas envolventes que inspiram a criatividade. "
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Originalidade e inovação ",
+                afirmacao: " *Além disso, os custos associados aos jogos, como hardware de alto desempenho e expansões, podem ser significativos, impactando o orçamento pessoal ou familiar. "
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = " Algumas vantagens e desvantagens dos jogos...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
